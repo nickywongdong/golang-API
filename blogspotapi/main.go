cbf743773585e028ad3dcabefc2f0a81f8be6fad
wongnich <wongnich@gmail.com>
2018-07-13 16:55:16 -0700

    creating global variable for database, and handling error upon opening database

--- blogspotapi/main.go
+++ blogspotapi/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func getBlogPosts(w http.ResponseWriter, req *http.Request) {
+//create global database variable to reference in endpoint functions
+var db *sql.DB
 
+//API endpont which gets all blog posts within the database
+func getBlogPosts(w http.ResponseWriter, req *http.Request) {
+	rows, _ := db.Query("Select post_id, title, body FROM blog")
 }
 
 func createBlogPost(w http.ResponseWriter, req *http.Request) {
@@ -16,10 +21,19 @@ func createBlogPost(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	//create router
 	router := mux.NewRouter()
 
-	router.HandleFunc("/blgs", getBlogPosts).Methods("GET")
-	router.HandleFunc("/blogs", createBlogPost).Methods("POST")
+	//open and use database blog.db file through sqlite3
+	var err error
+	db, err = sql.Open("sqlite3", "./blog.db")
+	if err != nil {
+		log.Fatal()
+	}
+
+	//reference functions as endpoints
+	router.HandleFunc("/posts", getBlogPosts).Methods("GET")
+	router.HandleFunc("/post", createBlogPost).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
