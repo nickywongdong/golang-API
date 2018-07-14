2b87373f782fcfbb4d8d0f561a5d4d32f5f40899
wongnich <wongnich@gmail.com>
2018-07-13 17:07:27 -0700

    adding functionality for getting all posts endpoint

--- blogspotapi/main.go
+++ blogspotapi/main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+//define a structure which all blog posts will need to adhere to:
+type Post struct {
+	post_id string `json:"post_id"`
+	title   string `json:"title"`
+	body    string `json:"body"`
+}
+
 //create global database variable to reference in endpoint functions
 var db *sql.DB
 
 //API endpont which gets all blog posts within the database
 func getBlogPosts(w http.ResponseWriter, req *http.Request) {
+	//create array which holds results:
+	var posts []Post
+
+	//select all rows from database with query command and store
 	rows, _ := db.Query("Select post_id, title, body FROM blog")
+
+	//iterate through rows, and append each to posts array to be returned later
+	var tempPost Post
+	for rows.Next() {
+		rows.Scan(&tempPost.post_id, &tempPost.title, &tempPost.body)
+		posts = append(posts, tempPost)
+	}
+
+	json.NewEncoder(w).Encode(posts)
 }
 
 func createBlogPost(w http.ResponseWriter, req *http.Request) {
